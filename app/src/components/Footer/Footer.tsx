3ace359650fc53eca5fbf8f23f60f317fa552e1c
serdeshnow <vserdes1977@mail.ru>
2024-11-14 01:27:06 +0300

    feat: complete footer & header

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -1,15 +1,31 @@
 import { Logo } from '../reusable';
+import { useEffect, useState } from 'react';
 
 export const Footer = () => {
+	const [city, setCity] = useState<string>('');
+	const [temperature, setTemperature] = useState<string>('');
+	const [weather, setWeather] = useState<string>('');
+
+	useEffect(() => {
+		fetch("https://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric&lang=ru&appid=61922dac724130ce4927ed7ad16a1e93")
+			.then((data) => data.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(String(main.temp));
+				setWeather(weather[0].description);
+		})
+	}, [])
+
 	return (
 		<footer className="xl:w-3/5 mx-10 w-5/6 flex-default text-center bg-green mb-1 py-1 px-2.5 rounded text-light-500 shadow-lg transition-all duration-300 ease-in-out">
 			<ul className="flex-default gap-5">
 				<li><Logo/></li>
 				<li>@Serdeshnow</li>
 			</ul>
-			<ul className="flex-default gap-5">
-				<li>date</li>
-				<li>map</li>
+			<ul className="flex-default gap-5 cursor-default">
+				<li>{new Date().toLocaleString('ru', {day: "numeric", month:"long"}) }</li>
+				<li>{city || null}</li>
+				<li>{temperature || null}C {weather || null}</li>
 			</ul>
 		</footer>
 	);
--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Icon, Logo } from '../reusable';
 import React from 'react';
 
 export const Header : React.FC = () => {
+	const navigate = useNavigate();
+
 	const isAuth: boolean = true;
 
 	return (
@@ -10,7 +12,7 @@ export const Header : React.FC = () => {
 			className="xl:w-3/5 mx-10 w-5/6 flex-default text-center bg-green mt-1 p-1 rounded text-light-500 shadow-lg transition-all duration-300 ease-in-out">
 			<ul className="flex-default gap-5">
 				<li className='green-hover-active'>
-					<button>
+					<button onClick={() => navigate(-1)}>
 						<i className="icon-back" style={{ fontSize: '25px' }} />
 					</button>
 				</li>
